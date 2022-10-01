9d6340cc321cfd7166ae4def424e998fdde137a8
jamesni <ni.ningning@gmail.com>
2022-10-01 05:04:03 +0000

    feat: jasypt integration

--- src/main/java/com/ningning/demo/DemoApplication.java
+++ src/main/java/com/ningning/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.ningning.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	@Value("${encrypted.property}")
+    private String property;
+
+    public String getProperty() {
+        return property;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hello %s!", this.property);
 	}
 }
